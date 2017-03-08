90c7991133c749c6e10ea4449aefb505c7f17b7d
fisal77 <fisal77@hotmail.com>
2017-03-08 22:27:56 +0300

    Corrects the errors in the User/Friend location on Map activity and layout. I will debug the other remaining errors....

--- app/src/main/java/com/seniorproject/sallemapp/Activities/NearByFragment.java
+++ app/src/main/java/com/seniorproject/sallemapp/Activities/NearByFragment.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.BaseBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.ListFragment;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
