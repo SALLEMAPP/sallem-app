5c3c72c5620fb2716ae6c14bdb5e97bbb2f7d72a
fisal77 <fisal77@hotmail.com>
2017-07-06 00:38:03 +0300

    Update

--- app/src/main/java/com/seniorproject/sallemapp/Activities/WelcomeActivity.java
+++ app/src/main/java/com/seniorproject/sallemapp/Activities/WelcomeActivity.java
@@ -4,17 +4,22 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.seniorproject.sallemapp.R;
+
 public class WelcomeActivity extends AppCompatActivity {
     private static final int REQUEST_SALLEM_PERMISSIONS = 123;
+    private AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +27,14 @@ public class WelcomeActivity extends AppCompatActivity {
         attachSigninButton();
         attachRegisterButton();
         getPermissions();
-
-
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-7249219499142063~4926980836");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("DFDC2A32E5ECB1E43EB3ADAEFB76B2FF")
+                .build();
+        boolean isTestDevice = adRequest.isTestDevice(this);
+        mAdView.loadAd(adRequest);
     }
 
     private void attachRegisterButton() {
